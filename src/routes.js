6ccb757f72c16838e3727a839d06d6aea8473b20
Fahad Sheikh <fahadsheikh780@gmail.com>
2016-12-13 03:21:00 -0800

    Update

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,6 @@ import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 import Layout from './components/Layout';
 import IndexPage from './components/IndexPage';
-import AthletePage from './components/AthletePage';
 import NotFoundPage from './components/NotFoundPage';
 
 const routes = (
